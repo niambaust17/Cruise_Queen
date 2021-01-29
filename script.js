5612f63f8dcd50ded9ed2e7dd91d328bd19a8043
unknown <niambaust17@gmail.com>
2021-01-30 01:26:01 +0600

    price calculation feature added

--- script.js
+++ script.js
@@ -11,4 +11,19 @@ function countTicket(className, isIncrement)
         specificTicketCountNew = specificTicketCountNew - 1;
     }
     document.getElementById(className + '-count').value = specificTicketCountNew;
+    ticketPriceCalculation();
+}
+
+
+function ticketPriceCalculation()
+{
+    const totalFirstClass = document.getElementById('firstClass-count').value;
+    const totalEconomyClass = document.getElementById('economyClass-count').value;
+    // console.log(totalFirstClass, totalEconomyClass);
+    const subTotal = totalFirstClass * 150 + totalEconomyClass * 100;
+    const vat = subTotal * 0.1;
+    const total = subTotal + vat;
+    document.getElementById('sub-total').innerText = '$' + subTotal;
+    document.getElementById('vat').innerText = '$' + vat;
+    document.getElementById('total-price').innerText = '$' + total;
 }
