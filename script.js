10fdbc3bfce085d83600aa4ab79f3cb2562f2452
unknown <niambaust17@gmail.com>
2021-01-30 13:13:23 +0600

    working on Book Now button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function countTicket(className, isIncrement)
 {
-    const specificTicketCount = document.getElementById(className + '-count').value;
+    const specificTicketCount = getInputValue(className);
     let specificTicketCountNew = parseInt(specificTicketCount);
     if (isIncrement == true)
     {
@@ -17,13 +17,37 @@ function countTicket(className, isIncrement)
 
 function ticketPriceCalculation()
 {
-    const totalFirstClass = document.getElementById('firstClass-count').value;
-    const totalEconomyClass = document.getElementById('economyClass-count').value;
-    // console.log(totalFirstClass, totalEconomyClass);
+    const totalFirstClass = getInputValue('firstClass');
+    const totalEconomyClass = getInputValue('economyClass');
+
     const subTotal = totalFirstClass * 150 + totalEconomyClass * 100;
     const vat = subTotal * 0.1;
     const total = subTotal + vat;
+
     document.getElementById('sub-total').innerText = '$' + subTotal;
     document.getElementById('vat').innerText = '$' + vat;
     document.getElementById('total-price').innerText = '$' + total;
+
+    // display user the summary of buying ticket after buying
+    document.getElementById('displayQuantityFirst').innerText = totalFirstClass;
+    document.getElementById('displayQuantityEconomy').innerText = totalEconomyClass;
+    document.getElementById('displayTotalFirst').innerText = '$' + totalFirstClass * 150;
+    document.getElementById('displayTotalEconomy').innerText = '$' + totalEconomyClass * 100;
+    document.getElementById('displaySubtotal').innerText = '$' + subTotal;
+    document.getElementById('displayVat').innerText = '$' + vat;
+    document.getElementById('displayTotal').innerText = '$' + total;
+}
+
+
+function getInputValue(className)
+{
+    const countTicket = document.getElementById(className + '-count').value;
+    return countTicket;
+}
+
+
+function bookingTicket()
+{
+    document.getElementById('booking-form').style.display = 'none';
+    document.getElementById('booking-info').style.display = 'block';
 }
